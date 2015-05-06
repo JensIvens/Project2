216240330328cd967669b4601c0dc642a7206779
Jens Ivens <jens.ivens@hotmail.com>
2015-05-06 14:57:36 +0200

    Fixing translation probs

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>We love bikes! - Login</title>
+	<title>We love bikes! - About</title>
 
 	<?php 
 		include('head.php');
@@ -23,8 +23,8 @@
 		</aside>
 		<div class="content">
 			<h2>About Us</h2>	
-			<p>'ProjectName' is a platform for bike-lovers. It's a very simple way to share your bike, or rent one by yourself! It's found in Mechelen, Belgium in february 2015. Currently ore than +5.000 bikes available and counting! </p>
-			<p>Whether an bike for an hour, a tandem for a week, or a mountainbike for a day, 'ProjectName' connects people to unique cycling experiences, at any price point, in more than 1,000 cities and 10 countries. And with world-class customer service and a growing community of users, 'ProjectName' is the easiest way for people to monetize their bike and showcase it to an audience of millions.</p>
+			<p>'ProjectName' is a platform for bike-lovers. It's a very simple way to share your bike, or rent one by yourself! It's found in Mechelen, Belgium in february 2015. Currently more than +5.000 bikes available and counting! </p>
+			<p>Whether an bike for an hour, a tandem for a week, or a mountainbike for a day, 'ProjectName' connects people to unique cycling experiences, at any price point, in more than 75 cities and 10 countries. And with world-class customer service and a growing community of users, 'ProjectName' is the easiest way for people to monetize their bike and showcase it to an audience of millions.</p>
 			<ul>
 				<li>
 					<i class="fa fa-users"></i>
