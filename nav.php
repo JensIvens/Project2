ea96e143ebc7b013df78e1117a935466a8bdd4b9
Jens Ivens <jens.ivens@hotmail.com>
2015-05-07 10:47:51 +0200

    Link fix

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <header>
 		<nav>
 			<ul id="menu">
-				<li><a href="#">Discover</a></li>
+				<li><a href="discover.php">Discover</a></li>
 				<li><a href="login.php">Login</a></li>
 				<li><a href="register.php">Register</a></li>	
 			</ul>
