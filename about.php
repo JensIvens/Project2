37c2e04b85596098d82500f7de00c0123a46b90d
Jens Ivens <jens.ivens@hotmail.com>
2015-05-07 10:46:30 +0200

    Content changed

--- about.php
+++ about.php
@@ -13,6 +13,7 @@
 	<?php 
 		include('nav.php');
 	 ?>
+	 
 	<div class="wrapper">
 		
 		<aside class="nav-sec">
@@ -23,8 +24,18 @@
 		</aside>
 		<div class="content">
 			<h2>About Us</h2>	
-			<p>'ProjectName' is a platform for bike-lovers. It's a very simple way to share your bike, or rent one by yourself! It's found in Mechelen, Belgium in february 2015. Currently more than +5.000 bikes available and counting! </p>
-			<p>Whether an bike for an hour, a tandem for a week, or a mountainbike for a day, 'ProjectName' connects people to unique cycling experiences, at any price point, in more than 75 cities and 10 countries. And with world-class customer service and a growing community of users, 'ProjectName' is the easiest way for people to monetize their bike and showcase it to an audience of millions.</p>
+			<p>
+				'ProjectName' is a platform for bike-lovers. It's a very simple way to share your bike, 
+				or rent one by yourself! It's found in Mechelen, Belgium in february 2015. 
+				Currently more than +5.000 bikes available and counting! 
+			</p>
+			<p>
+				Whether an bike for an hour, a tandem for a week, or a mountainbike for a day, 
+				'ProjectName' connects people to unique cycling experiences, at any price point, 
+				in more than 75 cities and 10 countries. And with world-class customer service 
+				and a growing community of users, 'ProjectName' is the easiest way for people 
+				to monetize their bike and showcase it to an audience of millions.
+			</p>
 			<ul>
 				<li>
 					<i class="fa fa-users"></i>
@@ -54,6 +65,8 @@
 	<?php 
 		include('footer.php') 
 	?>
-
+	<?php 
+		include('scripts.php');
+	?>
 </body>
 </html>
