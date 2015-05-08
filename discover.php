5bf9736516c17910f91b6818d822b6d011b2d1e0
Jens Ivens <jens.ivens@hotmail.com>
2015-05-08 15:46:58 +0200

    Discover - mobile version 1.0

--- discover.php
+++ discover.php
@@ -20,11 +20,42 @@
 
 	<div class="wrapper">
 		
-		<aside class="nav-sec">
-			
+		<aside class="filters">
+			<span>Filters <i class="fa fa-arrow-circle-down"></i></span>
 		</aside>
 		<div class="content">
-			
+			<ul class="discover-list">
+				<li class="discover-list-li">
+					<a href="#">
+						<img src="images/teaser1.png" />
+						<div class="discover-banner">
+	 						<img class="discover-marker" src="images/marker_map.png">
+	 						<p class="discover-name">Yannick Nijs</p>
+							<p class="discover-price">€ 8</p>
+						</div>
+					</a>
+				</li>
+				<li class="discover-list-li">
+					<a href="#">
+						<img src="images/teaser2.png" />
+						<div class="discover-banner">
+							<img class="discover-marker" src="images/marker_map.png">
+							<p class="discover-name">Yannick Nijs</p>
+							<p class="discover-price">€ 10</p>
+						</div>
+					</a>
+				</li>
+				<li class="discover-list-li">
+					<a href="#">
+						<img src="images/teaser3.png" />
+						<div class="discover-banner">
+ 							<img class="discover-marker" src="images/marker_map.png">
+ 							<p class="discover-name">Yannick Nijs</p>
+							<p class="discover-price">€ 12</p>
+						</div>
+					</a>
+				</li>
+			</ul>
 		</div>
 	</div>
 
