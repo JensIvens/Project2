fbf3e5f89abe05ab6a2e974ac2fbfc7f34952d8f
Jens Ivens <jens.ivens@hotmail.com>
2015-05-12 14:20:55 +0200

    Forgot password

---
+++ forgotpass.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>We love bikes! - Register</title>
+
+	<?php 
+		include('head.php');
+	?>
+
+</head>
+<body>
+	
+	<?php 
+		include('nav.php');
+	 ?>
+	
+	<section id="banner">
+		<div class="loginbox register">
+			<p>Reset password</p>
+			<form>
+				<span class="input-email-icon"><i class="fa fa-user"></i></span>
+				<input type="email" placeholder="Email:">
+				
+	    		<input type="submit" value="Send mail">
+			</form>
+		</div>
+	</section>
+	
+	<?php 
+		include('footer.php') 
+	?>
+	<?php 
+		include('scripts.php');
+	?>
+</body>
+</html>
